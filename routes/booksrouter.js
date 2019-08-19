d7e707305110a56f8ac3a73818154c0f36a7f19f
cristina1084 <cristina.johns1084@gmail.com>
2019-08-19 16:21:47 +0530

    CRUD operations

--- routes/booksrouter.js
+++ routes/booksrouter.js
@@ -1,8 +1,8 @@
 var exp = require('express');
 const router = exp.Router();    
-
+var bodyparser = require('body-parser');
 var books = require('../model/booksmodel');
-
+router.use(bodyparser.urlencoded({extended:true}));
 router.get("/",(req,res)=>{
     books.find({},(err,result)=>{
         if (err) throw err;
@@ -16,7 +16,8 @@ router.get("/",(req,res)=>{
                         {link:"/authors", title:"Authors"},
                         {link:"/signup", title:"Sign Up"},
                         {link:"/login", title:"Login"},
-                        {link:"/books/add", title:"Add Books"}
+                        {link:"/books/add", title:"Add Books"},
+                        {link:"/books/update", title:"Edit/Delete Books"}
                     ],
                     booksarr:result
                 }
@@ -34,10 +35,115 @@ router.get("/add",(req,res)=>{
                 {link:"/authors", title:"Authors"},
                 {link:"/signup", title:"Sign Up"},
                 {link:"/login", title:"Login"},
-                {link:"/books/add", title:"Add Books"}
+                {link:"/books/add", title:"Add Books"},
+                {link:"/books/update", title:"Edit/Delete Books"}
             ]
         }
     ); 
+})
+
+router.get("/update",(req,res)=>{
+    books.find({},(err,result)=>{
+        if (err) throw err;
+        else
+        res.render(
+            "updatebook",
+            {
+                pageTitle:"Library",
+                nav:[
+                    {link:"/books", title:"Books"}, 
+                    {link:"/authors", title:"Authors"},
+                    {link:"/signup", title:"Sign Up"},
+                    {link:"/login", title:"Login"},
+                    {link:"/books/add", title:"Add Books"},
+                    {link:"/books/update", title:"Edit/Delete Books"}
+                ],
+                booksarr:result
+            }
+        );
+    }) 
+})
+
+router.get("/update/:bid",(req,res)=>{
+    books.find({bookTitle:req.params.bid},(err,result)=>{
+        if (err) throw err;
+        else res.render(
+            "editbook",
+            {
+                pageTitle:"Library",
+                nav:[
+                    {link:"/books", title:"Books"}, 
+                    {link:"/authors", title:"Authors"},
+                    {link:"/signup", title:"Sign Up"},
+                    {link:"/login", title:"Login"},
+                    {link:"/books/add", title:"Add Books"},
+                    {link:"/books/update", title:"Edit/Delete Books"}
+                ],
+                book: result
+            });
+    })
+})
+
+router.post("/edit",(req,res)=>{
+    books.updateOne({bookTitle:req.body.title} ,{$set:{
+        bookTitle:req.body.title,
+        author : req.body.author,
+        genre : req.body.genre,
+        description : req.body.description,
+        price : req.body.price,
+        urlToImage : req.body.url
+    }}, (err,result)=>{
+        if (err) throw err;
+        else{
+            books.find({},(err,result)=>{
+                if (err) throw err;
+                else
+                res.render(
+                    "books",
+                    {
+                        pageTitle:"Library",
+                        nav:[
+                            {link:"/books", title:"Books"}, 
+                            {link:"/authors", title:"Authors"},
+                            {link:"/signup", title:"Sign Up"},
+                            {link:"/login", title:"Login"},
+                            {link:"/books/add", title:"Add Books"},
+                            {link:"/books/update", title:"Edit/Delete Books"}
+                        ],
+                        booksarr:result
+                    }
+                );
+            })
+        }
+    }) 
+})
+
+router.get("/delete/:bid",(req,res)=>{
+    books.deleteOne({bookTitle:req.params.bid},(err,result)=>{
+        if (err) throw err;
+        else
+        {
+            books.find({},(err,result)=>{
+                if(err) throw err;
+                else
+                res.render(
+                    "updatebook",
+                    {
+                        pageTitle:"Library",
+                        nav:[
+                            {link:"/books", title:"Books"}, 
+                            {link:"/authors", title:"Authors"},
+                            {link:"/signup", title:"Sign Up"},
+                            {link:"/login", title:"Login"},
+                            {link:"/books/add", title:"Add Books"},
+                            {link:"/books/update", title:"Edit/Delete Books"}
+                        ],
+                        booksarr:result
+                    }
+                );
+            })
+        }
+    })
 })
 
 router.get("/:id",(req,res)=>{
@@ -53,7 +159,8 @@ router.get("/:id",(req,res)=>{
                         {link:"/authors", title:"Authors"},
                         {link:"/signup", title:"Sign Up"},
                         {link:"/login", title:"Login"},
-                        {link:"/books/add", title:"Add Books"}
+                        {link:"/books/add", title:"Add Books"},
+
                     ],
                     book:result[req.params.id]
                 }
